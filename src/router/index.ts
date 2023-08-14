5b42dc76dc45f0daf7a00cbc8c95f612df776238
Saw <ehkalanephaung96@gmail.com>
2023-08-14 22:34:17 +0700

    import teacher card

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import StudentView from '../views/StudentView.vue'
 import StudentDetailView from '../views/StudentDetailView.vue'
 import StudentLayoutView from '../views/StudentLayoutView.vue'
+import TeacherView from '../views/TeacherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
       path: '/',
       name: 'students',
       component: StudentView
+    },
+    {
+      path: '/teacher',
+      name: 'teacher',
+      component: TeacherView
     },
     {
       path: '/student/:id',
