ba382e634f3d23633a0255eb685f95c57f4d806b
Saw <ehkalanephaung96@gmail.com>
2023-08-14 22:38:30 +0700

    Create advisor service

---
+++ src/services/AdvisorService.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import type { AxiosInstance, AxiosResponse } from 'axios'
+import type { TeacherDetail } from '@/type'
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:3004',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+export default {
+  getAdvisor(): Promise<AxiosResponse<TeacherDetail[]>> {
+    return apiClient.get<TeacherDetail[]>('/advisor')
+  },
+  getAdvisorById(id: number): Promise<AxiosResponse<TeacherDetail>> {
+    return apiClient.get<TeacherDetail>('advisor/' + id.toString())
+  }
+}
