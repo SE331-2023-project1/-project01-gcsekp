626c8367743946ad200f8b36adfb8867d14d5723
Saw <ehkalanephaung96@gmail.com>
2023-08-14 13:24:15 +0700

    Add study interface

---
+++ src/type.ts
@@ -0,0 +1,9 @@
+export interface StudentDetail {
+  id: number
+  firstName: string
+  lastName: string
+  studentID: string
+  registeredCourse: string
+  advisor: number
+  studentPicture: string
+}
