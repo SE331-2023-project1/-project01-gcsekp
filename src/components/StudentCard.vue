f9252c28b97a4d30fc9bfb0dcbf6dd23dac1d54f
Saw <ehkalanephaung96@gmail.com>
2023-08-14 16:04:23 +0700

    create studentDetail and install Tailwind CSS

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -8,30 +8,22 @@
     <div v-if="student" class="StudentDetail-class">
       <span> First Name: {{ student.FirstName }}</span>
       <p>Last Name: {{ student.LastName }}</p>
-      <!-- <p>Student ID: {{ student.Student_ID}}</p>
-      <p>Registered: {{ student.registered }}</p>
-      <p>Advisor: {{ student.advisor}}</p> -->
       <img :src="student.stu_pic" alt="img" />
     </div>
   </RouterLink>
 </template>
 
 <script setup lang="ts">
-// add student detals
+import { ref, type PropType } from 'vue'
+import { type StudentDetail } from '@/type'
+
+const StudentDetail = ref({})
+const props = defineProps({
+  student: {
+    type: Object as PropType<StudentDetail>,
+    required: true
+  }
+})
 </script>
 
-<style scoped>
-/* .StudentDetail-class {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1 px solid #39495c;
-  margin-top: 20px;
-  margin-bottom: 18px;
-}
-
-.StudentDetail-class:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-} */
-</style>
+<style scoped></style>
---
+++ src/views/StudentDetailView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import type { StudentDetail } from '@/type'
+import { ref, type PropType } from 'vue'
+import StudentService from '@/services/StudentService'
+
+defineProps({
+  student: {
+    type: Object as PropType<StudentDetail>,
+    require: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <div v-if="student">
+      <p><span class="font-bold">First Name :</span> {{ student.firstName }}</p>
+      <p><span class="font-bold">Last Name :</span> {{ student.lastName }}</p>
+      <p><span class="font-bold">Student ID :</span> {{ student.studentID }}</p>
+      <p><span class="font-bold mb-10">Registered on :</span> {{ student.registeredCourses }}</p>
+    </div>
+  </div>
+</template>
