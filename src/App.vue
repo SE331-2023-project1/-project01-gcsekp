7951aabf542713f3007726fd6b45345baf9926b0
Saw <ehkalanephaung96@gmail.com>
2023-08-14 23:24:45 +0700

    Apply style in home page

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Student</RouterLink>
-      <RouterLink to="/about">advisor</RouterLink>
-    </nav>
+    <img alt="Vue logo" class="logo" src="@/assets/management.png" width="125" height="125" />
+    <div class="wrapper">
+      <nav class="font-mono text-yellow-300">
+        <RouterLink to="/">Student</RouterLink> |
+        <RouterLink to="/about">advisor</RouterLink>
+      </nav>
+    </div>
   </header>
 
   <RouterView />
--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -2,12 +2,12 @@
   <div
     class="StudentDetail-class border-2 border-blue-950 w20-h20 rounded-lg shadow-lg m-5 bg-white px-10 py-8 text-blue-950 text-base font-mono hover:bg-blue-50 cursor-pointer"
   >
-    <span> First Name: {{ student.FirstName }}</span>
+    <span> First Name: {{ student.firstName }}</span>
   </div>
   <RouterLink :to="{ name: 'student-detail', params: { id: student.id } }">
     <div v-if="student" class="StudentDetail-class">
-      <span> First Name: {{ student.FirstName }}</span>
-      <p>Last Name: {{ student.LastName }}</p>
+      <span> First Name: {{ student.firstName }}</span>
+      <p>Last Name: {{ student.lastName }}</p>
       <img :src="student.stu_pic" alt="img" />
     </div>
   </RouterLink>
