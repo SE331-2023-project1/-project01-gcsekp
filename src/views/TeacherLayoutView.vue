11e0b056e7fd00f65f40192b6bbf082125a89b69
GC632115001 <poison45lol@gmail.com>
2023-08-18 17:19:31 +0700

    continue TeacherLayoutView

---
+++ src/views/TeacherLayoutView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import type { StudentDetail } from '@/type'
+import { ref, type PropType } from 'vue'
+import AdvisorService from '@/services/AdvisorService'
+import { useRouter } from 'vue-router'
+
+const student = ref<StudentDetail | null>(null)
+const router = useRouter()
+
+const props = defineProps({
+  id: String
+})
+
+AdvisorService.getStudentById(Number(props.id))
+  .then((response) => {
+    advisor.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+    if (error.response && error.response.status === 404) {
+      router.push({ name: '404-resource', params: { resource: 'advisor' } })
+    } else {
+      router.push({ name: 'network-error' })
+    }
+  })
+</script>
+
+<template>
+  <div>
+    <div v-if="advisor">
+      <img class="mb-5" :src="student.stu_pic" alt="img" />
+      <hr class="mb-5" />
+      <div id="nav">
+        <RouterLink
+          class="w-1/2 mr-3 text-green-500 py-2 rounded text-center"
+          :to="{ name: 'student-detail', params: { id } }"
+          >Details</RouterLink
+        >
+      </div>
+    </div>
+    <RouterView class="mt-3" :student="student"></RouterView>
+  </div>
+</template>
