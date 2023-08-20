1315302b86eeb59db89dfeb48e3abeeb7ffc0532
Saw <ehkalanephaung96@gmail.com>
2023-08-20 22:25:35 +0700

    add props

--- src/views/TeacherDetailView.vue
+++ src/views/TeacherDetailView.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
 import type { Ref } from 'vue'
 import type { TeacherDetail } from '@/type'
-import { ref, type PropType } from 'vue'
-import AdvisorService from '@/services/AdvisorService' 
+// import { ref, type PropType } from 'vue'
+import { ref, defineProps } from 'vue'
+import AdvisorService from '@/services/AdvisorService'
 
-defineProps({
+const props = defineProps({
   advisor: {
-    type: Object as PropType<TeacherDetail>,
-    require: true
+    type: Object as () => TeacherDetail,
+    required: true
   }
 })
-
 </script>
 
 <template>
-    <p>Registration from here</p>
-    <div>
-      <div v-if="advisor">
-        <p><span class="font-bold">First Name :</span> {{ advisor.FirstName }}</p>
-        <p><span class="font-bold">Last Name :</span> {{ advisor.LastName }}</p>
-        <p><span class="font-bold">Student ID :</span> {{ advisor.Office }}</p>
-        <p><span class="font-bold mb-10">Registered on :</span> {{ advisor.Email }}</p>
-      </div>
+  <p>Registration from here</p>
+  <div>
+    <div v-if="advisor">
+      <p><span class="font-bold">First Name :</span> {{ props.advisor.FirstName }}</p>
+      <p><span class="font-bold">Last Name :</span> {{ props.advisor.LastName }}</p>
+      <p><span class="font-bold">Department :</span> {{ props.advisor.Department }}</p>
+      <p><span class="font-bold mb-10">Registered on :</span> {{ props.advisor.Email }}</p>
     </div>
-  </template>
+  </div>
+</template>
