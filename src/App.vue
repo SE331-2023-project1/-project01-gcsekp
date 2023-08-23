a30e7af64b5714985f5def433b78a19e14972e5f
Saw <ehkalanephaung96@gmail.com>
2023-08-23 20:49:46 +0700

    adject style

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,15 @@
 }
 
 header {
-  position: sticky;
-  top: 0;
   z-index: 100;
-  /* background-color: white; */
   padding: 10px 0;
   box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);
 }
 
 .wrapper {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
 
 nav {
